557d318669a9b686cfe6f86a296fe7ad1f493200
Beyonce Knowles <you@example.com>
2021-02-21 12:59:06 -0500

    starting header

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <main className='App'>
-      <p>Welcome to my Algorand Interest tracking page</p>
+      <div className='main'>
+        <h1>Welcome to my Algorand Interest tracking page</h1>
+      </div>
     </main>
   );
 }
