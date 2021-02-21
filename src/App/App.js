db1a9b002c6091b05f6761e89a0a8f26e312cc09
Beyonce Knowles <you@example.com>
2021-02-21 14:28:41 -0500

    add success page

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header/header';
-import Router from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Success from '../Success/success';
 
 function App() {
   return (
+    
     <main className='App'><br /><br />
       <Header /><br />
       <div className='main'>
         <h1>Welcome to my Algorand Interest tracking page</h1>
       </div>
+    <Route exact path='/success' component={Success} />
     </main>
+    
   );
 }
 
---
+++ src/Success/success.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './success.css';
+
+export default function Success() {
+  return (
+    <div className='success'>
+      <p>Here is the success page</p>
+    </div>
+  )
+}
